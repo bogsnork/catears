ffccd1303507b50b9134c182fc4f846e9dee0fb0
nogirvine <43342463+nogirvine@users.noreply.github.com>
2019-04-11 17:15:13 +0100

    Update catears.ino

--- catears.ino
+++ catears.ino
@@ -77,7 +77,7 @@ void loop() {
     }
 
   }
-  else if (button2State = HIGH) {
+  else if (button2State == HIGH) {
     digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
     delay(1000);                       // wait for a second
     digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
